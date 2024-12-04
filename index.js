3ac93de3e91276a23a50f36a812b5101f401fc9b
Richard McCormack <richard.mccormack@wpengine.com>
2024-12-04 11:48:59 +0000

    Second commit, delete node modules & add basic scaffolding

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/webhook', (req, res) => {
+    console.log('Received Webhook:', req.body);
+    res.status(200).send('OK');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Webhook receiver listening on port ${PORT}`);
+});
