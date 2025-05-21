c2be20d80555b0dbe187525bbd522e4c8d9cda54
richard-wpengine <richard.mccormack@wpengine.com>
2025-05-21 12:15:49 +0100

    feat: 🎸 Setup redirect with delay fro report

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
+var count = 0;
 
-app.get('/webhook', (req, res) => {
+app.get('/webhook', async (req, res) => {
     console.log('Received Webhook:', req.body);
-    res.status(200).send('OK');
+    // run script, return something with delay
+    await setTimeout(() => {
+        res.status(200).redirect('/report');
+    }, 5000); // Simulate a delay of 5 seconds
+});
+app.get('/report', async (req, res) => {
+    console.log('Received Redirect:', req.body);
+    // trigger PW report for CWV score & replace when done 
+    await setTimeout(() => {
+        res.status(301).send(`<html><body><div><h3>Report Page - Attempt ${++count}</h1></div><br/><div id="report-body"><span>Report building...</span></div></body></html>`);
+    }, 5000); // Simulate a delay of 5 seconds
 });
 
 const PORT = process.env.PORT || 3000;
